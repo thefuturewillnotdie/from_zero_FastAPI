797ffc408093f063d705feb74105d14a32cc1f8e
Maksim Pantsialeyeu <invinciblefromrepbel@proton.me>
2025-12-28 17:50:01 +0300

    many-to-many-post-tags added, adding tag to a post added

--- app/models.py
+++ app/models.py
@@ -1,6 +1,14 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from app.database import Base
+
+PostTag = Table(
+    "posts_tags",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("post_id", ForeignKey("posts.id")),
+    Column("tag_id", ForeignKey("tags.id")),
+)
 
 
 class User(Base):
@@ -26,3 +34,16 @@ class Post(Base):
     )
 
     user = relationship("User", back_populates="posts")
+
+    tags = relationship(
+        "Tag", secondary=PostTag, back_populates="posts", cascade="all, delete"
+    )
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    id = Column(Integer, primary_key=True, index=True)
+    word = Column(String, nullable=False)
+
+    posts = relationship("Post", secondary=PostTag, back_populates="tags")
